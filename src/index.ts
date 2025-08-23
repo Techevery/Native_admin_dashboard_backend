25a7f3fa548c962e73e79ab542f19dc3cc2462c8
fevico <ajayifavour81@gmail.com>
2025-08-23 10:16:02 +0100

    port changes fix

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ app.use("/auth", authRouter)
     
 dbConnect()
  
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => { 
     console.log(`Server is running on http://localhost:${PORT}`);   
 })  
